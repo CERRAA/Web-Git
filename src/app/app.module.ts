d45b6eb64d7f1035340438c64cdb0e5ce27f7a6f
CERRAA <cerraangie113@gmail.com>
2022-04-20 10:35:58 +0300

    added form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { UserComponent } from './user/user.component';
+import { RepoComponent } from './repo/repo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    UserComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule,
